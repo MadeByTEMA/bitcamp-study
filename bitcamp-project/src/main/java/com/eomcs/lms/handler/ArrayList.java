1752d6c88f5c732824c0a7704ee96a7ba2cb8dc7
TEMA <tema_@naver.com>
2020-01-02 17:11:58 +0900

    2020.01.02

--- bitcamp-project/src/main/java/com/eomcs/lms/handler/ArrayList.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/handler/ArrayList.java
@@ -2,23 +2,23 @@ package com.eomcs.lms.handler;
 
 import java.util.Arrays;
 import com.eomcs.lms.domain.Board;
-public class BoardList {
+public class ArrayList {
 
   static final int DEFAULT_CAPACITY = 3;
-  Board[] list;
+  Object[] list;
   int size;
 
-  public BoardList() {
-    this.list = new Board[DEFAULT_CAPACITY];
+  public ArrayList() {
+    this.list = new Object[DEFAULT_CAPACITY];
   }
 
-  public BoardList(int capacity) {
+  public ArrayList(int capacity) {
     if (capacity < DEFAULT_CAPACITY || capacity > 10000)
-      this.list = new Board[DEFAULT_CAPACITY];
+      this.list = new Object[DEFAULT_CAPACITY];
     else
-      this.list = new Board[capacity];
+      this.list = new Object[capacity];
   }
-  public Board[] toArray() {
+  public Object[] toArray() {
     /*
     Board[] arr = new Board[this.size];
     for (int i = 0; i < this.size; i++) {
@@ -29,7 +29,7 @@ public class BoardList {
     return Arrays.copyOf(this.list, this.size);
   }
 
-  public void add(Board board) {
+  public void add(Object obj) {
     if(this.size == this.list.length) {
       int oldCapacity = this.list.length;
       int newCapacity = oldCapacity + (oldCapacity >> 1);
@@ -42,15 +42,14 @@ public class BoardList {
        */
       this.list = Arrays.copyOf(this.list, newCapacity);
     }
-    this.list[this.size++] = board;
+    this.list[this.size++] = obj;
   }
 
-  public Board get(int no) {
-    for (int i = 0; i < this.size; i++) {
-      if (this.list[i].getNo() == no) {
-        return this.list[i];
-      }
+  public Object get(int idx) { 
+    if (idx >= 0 && idx < this.size) {
+      return this.list[idx];
+    } else {
+      return null;
     }
-    return null;
   }
 }
