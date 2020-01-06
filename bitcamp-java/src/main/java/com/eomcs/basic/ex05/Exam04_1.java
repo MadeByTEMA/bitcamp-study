d18a54a40f118c3ed8e22ac7361475941e01f89d
TEMA <tema_@naver.com>
2020-01-06 18:15:29 +0900

    2020.01.06

--- bitcamp-java/src/main/java/com/eomcs/basic/ex05/Exam04_1.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex05/Exam04_1.java
@@ -16,6 +16,7 @@ public class Exam04_1 {
         System.out.println(i << 1); // 22 => 00010110
         System.out.println(i << 2); // 44 => 00101100
         System.out.println(i << 3); // 88 => 01011000
+        
     }
 }
 
