fa53c32e10692dd83eea6f87da51e2fcc99b3c21
TEMA <tema_@naver.com>
2020-01-13 14:27:07 +0900

    2020.01.13

---
+++ bitcamp-project/v21/src/main/java/com/eomcs/util/StackTest.java
@@ -0,0 +1,15 @@
+package com.eomcs.util;
+
+public class StackTest {
+  
+  public void main(String[] args) {
+    
+    Stack stack = new Stack();
+    
+    stack.push("aaa");
+    stack.push("bbb");
+    stack.push("ccc");
+    stack.push("ddd");
+    
+  }
+}
