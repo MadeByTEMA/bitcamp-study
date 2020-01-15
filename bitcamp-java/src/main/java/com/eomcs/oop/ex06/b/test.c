10a3f3ce734bb55faa59c22dbc48813bb34160db
TEMA <tema_@naver.com>
2020-01-15 16:41:01 +0900

    2020.01.15

--- bitcamp-java/src/main/java/com/eomcs/oop/ex06/b/test.c
+++ bitcamp-java/src/main/java/com/eomcs/oop/ex06/b/test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-float eom_plusf(float a, float b) {
+float ke_plusF(float a, float b) {
     return a + b;
 }
 
-int eom_plusi(int a, int b) {
+int ke_plusI(int a, int b) {
     return a + b;
 }
 
 int main(void) {
     printf("Hello!\n");
     
-    int r = eom_plusi(100, 200);
+    int r = ke_plusI(100, 200);
     printf("r = %d\n", r);
     
-    float r2 = eom_plusf(100.5f, 200.2f);
+    float r2 = ke_plusF(100.5f, 200.2f);
     printf("r2 = %f\n", r2);
 } 
 
