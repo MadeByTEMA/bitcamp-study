cbba4a5ebb197b4777edd527c88b39447bb6cb10
TEMA <tema_@naver.com>
2020-02-21 12:39:59 +0900

    2020.02.21

--- bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/ClientApp.java
@@ -1,37 +1,13 @@
 // LMS 클라이언트
 package com.eomcs.lms;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-import com.eomcs.lms.dao.BoardDao;
-import com.eomcs.lms.dao.LessonDao;
-import com.eomcs.lms.dao.MemberDao;
-import com.eomcs.lms.dao.mariadb.BoardDaoImpl;
-import com.eomcs.lms.dao.mariadb.LessonDaoImpl;
-import com.eomcs.lms.dao.mariadb.MemberDaoImpl;
-import com.eomcs.lms.handler.BoardAddCommand;
-import com.eomcs.lms.handler.BoardDeleteCommand;
-import com.eomcs.lms.handler.BoardDetailCommand;
-import com.eomcs.lms.handler.BoardListCommand;
-import com.eomcs.lms.handler.BoardUpdateCommand;
 import com.eomcs.lms.handler.Command;
-import com.eomcs.lms.handler.LessonAddCommand;
-import com.eomcs.lms.handler.LessonDeleteCommand;
-import com.eomcs.lms.handler.LessonDetailCommand;
-import com.eomcs.lms.handler.LessonListCommand;
-import com.eomcs.lms.handler.LessonUpdateCommand;
-import com.eomcs.lms.handler.MemberAddCommand;
-import com.eomcs.lms.handler.MemberDeleteCommand;
-import com.eomcs.lms.handler.MemberDetailCommand;
-import com.eomcs.lms.handler.MemberListCommand;
-import com.eomcs.lms.handler.MemberUpdateCommand;
 import com.eomcs.util.Prompt;
 
 public class ClientApp {
@@ -42,46 +18,10 @@ public class ClientApp {
   Deque<String> commandStack;
   Queue<String> commandQueue;
 
-  Connection con;
-
-  HashMap<String, Command> commandMap = new HashMap<>();
 
   public ClientApp() throws Exception {
-    // 생성자?
-    // => 객체가 작업할 때 사용할 자원들을 준비하는 일을 한다.
-
-    // 사용자가 입력한 명령어를 보관할 객체 준비
     commandStack = new ArrayDeque<>();
     commandQueue = new LinkedList<>();
-
-    // DB 연결 객체 준비
-    Class.forName("org.mariadb.jdbc.Driver");
-    con = DriverManager.getConnection( //
-        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
-
-    // MariaDB와 연동하여 데이터를 처리하는 DAO 객체 준비
-    BoardDao boardDao = new BoardDaoImpl(con);
-    MemberDao memberDao = new MemberDaoImpl(con);
-    LessonDao lessonDao = new LessonDaoImpl(con);
-
-    // 사용자 명령을 처리할 Command 객체 준비
-    commandMap.put("/board/list", new BoardListCommand(boardDao));
-    commandMap.put("/board/add", new BoardAddCommand(boardDao, prompt));
-    commandMap.put("/board/detail", new BoardDetailCommand(boardDao, prompt));
-    commandMap.put("/board/update", new BoardUpdateCommand(boardDao, prompt));
-    commandMap.put("/board/delete", new BoardDeleteCommand(boardDao, prompt));
-
-    commandMap.put("/lesson/list", new LessonListCommand(lessonDao));
-    commandMap.put("/lesson/add", new LessonAddCommand(lessonDao, prompt));
-    commandMap.put("/lesson/detail", new LessonDetailCommand(lessonDao, prompt));
-    commandMap.put("/lesson/update", new LessonUpdateCommand(lessonDao, prompt));
-    commandMap.put("/lesson/delete", new LessonDeleteCommand(lessonDao, prompt));
-
-    commandMap.put("/member/list", new MemberListCommand(memberDao));
-    commandMap.put("/member/add", new MemberAddCommand(memberDao, prompt));
-    commandMap.put("/member/detail", new MemberDetailCommand(memberDao, prompt));
-    commandMap.put("/member/update", new MemberUpdateCommand(memberDao, prompt));
-    commandMap.put("/member/delete", new MemberDeleteCommand(memberDao, prompt));
   }
 
   public void service() {
@@ -111,7 +51,6 @@ public class ClientApp {
     keyboard.close();
 
     try {
-      con.close();
     } catch (Exception e) {
     }
   }
