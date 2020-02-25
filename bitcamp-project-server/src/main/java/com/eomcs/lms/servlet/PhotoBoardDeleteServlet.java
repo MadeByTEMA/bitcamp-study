55580b6475373f6a03ce9e7a132c7bb4900e5c62
TEMA <tema_@naver.com>
2020-02-25 16:08:44 +0900

    2020.02.25

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/PhotoBoardDeleteServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/PhotoBoardDeleteServlet.java
@@ -3,24 +3,27 @@ package com.eomcs.lms.servlet;
 import java.io.PrintStream;
 import java.util.Scanner;
 import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.util.Prompt;
 
 public class PhotoBoardDeleteServlet implements Servlet {
 
   PhotoBoardDao photoBoardDao;
+  PhotoFileDao photoFileDao;
 
-  public PhotoBoardDeleteServlet(PhotoBoardDao photoBoardDao) {
+  public PhotoBoardDeleteServlet( //
+      PhotoBoardDao photoBoardDao, //
+      PhotoFileDao photoFileDao) {
     this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
   }
 
 
   @Override
   public void service(Scanner in, PrintStream out) throws Exception {
 
-    out.println("번호? ");
-    out.println("!{}!");
-    out.flush();
-
-    int no = Integer.parseInt(in.nextLine());
+    int no = Prompt.getInt(in, out, "번호? ");
+    photoFileDao.deleteAll(no);
 
     if (photoBoardDao.delete(no) > 0) {
       out.println("사진 게시글을 삭제했습니다.");
