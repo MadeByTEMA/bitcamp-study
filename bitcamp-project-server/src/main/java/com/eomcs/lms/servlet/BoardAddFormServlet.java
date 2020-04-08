06df26490bf60e28ec89128b127ce69da0577c49
TEMA <tema_@naver.com>
2020-04-08 15:15:59 +0900

    2020.04.08

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardAddFormServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardAddFormServlet.java
@@ -1,13 +1,11 @@
 package com.eomcs.lms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 // @Component // Spring IoC 가 관리
 @WebServlet("/board/addForm") // 서블릿 컨테이너가 이 객체를 관리한다.
@@ -20,27 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 // => javax.servlet.Servlet 인터페이스를 구현하라!
 // => 또는 이 인터페이스를 미리 구현한 javax.servlet.GenericServlet을 상속 받아라!
 //
-public class BoardAddFormServlet extends HttpServlet {
+public class BoardAddFormServlet extends GenericServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset='UTF-8'>");
-    out.println("<title>게시글 입력</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>게시물 입력</h1>");
-    out.println("<form action='add'>");
-    out.println("내용:<br>");
-    out.println("<textarea name='title' rows='5' cols='60'></textarea><br>");
-    out.println("<button>등록</button>");
-    out.println("</form>");
-    out.println("</body>");
-    out.println("</html>");
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+
   }
 }
--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardAddServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/BoardAddServlet.java
@@ -2,16 +2,13 @@ package com.eomcs.lms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.context.ApplicationContext;
-
 import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.service.BoardService;
 
@@ -20,7 +17,8 @@ public class BoardAddServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     out.println("<!DOCTYPE html>");
@@ -41,13 +39,16 @@ public class BoardAddServlet extends HttpServlet {
   }
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
     try {
+      request.setCharacterEncoding("UTF-8");
       response.setContentType("text/html;charset=UTF-8");
       PrintWriter out = response.getWriter();
 
-      ServletContext servletContext = request.getServletContext();
-      ApplicationContext iocContainer = (ApplicationContext) servletContext.getAttribute("iocContainer");
+      ServletContext servletContext = getServletContext();
+      ApplicationContext iocContainer =
+          (ApplicationContext) servletContext.getAttribute("iocContainer");
       BoardService boardService = iocContainer.getBean(BoardService.class);
 
       Board board = new Board();
