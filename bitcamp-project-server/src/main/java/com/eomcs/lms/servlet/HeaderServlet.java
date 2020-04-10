9a97e51c80d8fd04b7a63367331cee2a1f12477c
TEMA <tema_@naver.com>
2020-04-10 10:31:23 +0900

    2020.04.10

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/HeaderServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/HeaderServlet.java
@@ -2,12 +2,12 @@ package com.eomcs.lms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.lms.domain.Member;
 
 @WebServlet("/header")
 public class HeaderServlet extends HttpServlet {
@@ -39,14 +39,14 @@ public class HeaderServlet extends HttpServlet {
     out.println("</style>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<nav class='navbar navbar-expand-lg navbar-light bg-light'>");
+    out.println("<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>");
     out.println("<a class='navbar-brand' href='#'>Navbar</a>");
     out.println(
         "<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>");
     out.println("  <span class='navbar-toggler-icon'></span>");
     out.println("</button>");
     out.println("<div class='collapse navbar-collapse' id='navbarNav'>");
-    out.println("  <ul class='navbar-nav'>");
+    out.println("  <ul class='navbar-nav mr-auto'>");
     out.println("    <li class='nav-item'>");
     out.println("      <a class='nav-link' href='../board/list'>게시글</span></a>");
     out.println("    </li>");
@@ -60,9 +60,14 @@ public class HeaderServlet extends HttpServlet {
     out.println("      <a class='nav-link' href='../auth/login'>로그인</a>");
     out.println("    </li>");
     out.println("  </ul>");
-    out.println("<span class='navbar-test'>");
-    out.println("홍길동");
-    out.println("</span>");
+    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    if (loginUser != null) {
+      out.printf("  <span class='navbar-text'>%s</span>\n", //
+          loginUser.getName());
+      out.println("  <a href='../auth/logout' class='btn btn-success btn-sm'>로그아웃</a>");
+    } else {
+      out.println("  <a href='../auth/login' class='btn btn-success btn-sm'>로그인</a>");
+    }
     out.println("</div>");
     out.println("</nav>");
     out.println("<div class='container'>");
