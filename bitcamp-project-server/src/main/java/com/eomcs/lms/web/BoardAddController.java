dd19dd1899775e656e8f2003df369ee60c570437
TEMA <tema_@naver.com>
2020-04-14 18:03:15 +0900

    2020.04.14

--- bitcamp-project-server/src/main/java/com/eomcs/lms/web/BoardAddController.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/web/BoardAddController.java
@@ -1,6 +1,5 @@
 package com.eomcs.lms.web;
 
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +9,20 @@ import com.eomcs.lms.service.BoardService;
 import com.eomcs.util.RequestMapping;
 
 @Component
-public class BoardListController {
+public class BoardAddController {
 
   @Autowired
   BoardService boardService;
 
-  @RequestMapping("/board/list")
-  public String list(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    List<Board> boards = boardService.list();
-    request.setAttribute("list", boards);
-    return "/board/list.jsp";
+  @RequestMapping("/board/add")
+  public String add(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    if (request.getMethod().equals("GET")) {
+      return "/board/form.jsp";
+    }
+
+    Board board = new Board();
+    board.setTitle(request.getParameter("title"));
+    boardService.add(board);
+    return "redirect:list";
   }
 }
--- bitcamp-project-server/src/main/java/com/eomcs/lms/web/BoardDetailController.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/web/BoardDetailController.java
@@ -1,6 +1,5 @@
 package com.eomcs.lms.web;
 
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +9,16 @@ import com.eomcs.lms.service.BoardService;
 import com.eomcs.util.RequestMapping;
 
 @Component
-public class BoardListController {
+public class BoardDetailController {
 
   @Autowired
   BoardService boardService;
 
-  @RequestMapping("/board/list")
-  public String list(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    List<Board> boards = boardService.list();
-    request.setAttribute("list", boards);
-    return "/board/list.jsp";
+  @RequestMapping("/board/detail")
+  public String detail(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    int no = Integer.parseInt(request.getParameter("no"));
+    Board board = boardService.get(no);
+    request.setAttribute("board", board);
+    return "/board/detail.jsp";
   }
 }
