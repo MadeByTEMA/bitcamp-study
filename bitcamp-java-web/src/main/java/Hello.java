5f8a756e6f7b0fb8f7bc43301f200c2b94efdca6
TEMA <tema_@naver.com>
2020-05-07 12:05:30 +0900

    2020.05.07

--- bitcamp-java-web/src/main/java/Hello.java
+++ bitcamp-java-web/src/main/java/Hello.java
@@ -1,19 +1,6 @@
-
 public class Hello {
 
-  { // 인스턴스 블록
-    System.out.println("ex06 인스턴스 생성!");
-  }
+  public static void main(String[] args) throws Exception {}
 
-  static { // 스태틱 블록
-    System.out.println("ex06 클래스 로딩");
-  }
-
-  public static void main(String[] args) {
-    int a = 10;
-    if (a < 100) {
-      System.out.println("okok");
-    }
-  }
 
 }
